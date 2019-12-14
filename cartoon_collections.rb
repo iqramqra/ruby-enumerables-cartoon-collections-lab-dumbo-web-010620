6fb4ac76a86718f42730617c2c4a1bad594bfc6b
Iqra Masroor <masrooriqra@gmail.com>
2019-12-14 01:19:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,6 @@ def long_planeteer_calls(array_of_calls)
 end
 
 def find_the_cheese(string)
-  string.find do |cheese|
-    
+  string.find
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
